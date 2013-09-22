057d644190550f49559d6535d8829c91e1448089
treken <chupekevin@gmail.com>
2013-09-21 21:21:45 -0400

    Modded for GE launcher hotseat layout

--- Android.mk
+++ Android.mk
@@ -17,13 +17,20 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := user
+LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_STATIC_JAVA_LIBRARIES := android-common android-support-v13
 
-LOCAL_PACKAGE_NAME := Launcher
+LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-renderscript-files-under, src)
+LOCAL_SDK_VERSION := current
+
+LOCAL_PACKAGE_NAME := Launcher2
 LOCAL_CERTIFICATE := shared
 
 LOCAL_OVERRIDES_PACKAGES := Home
 
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
 include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
